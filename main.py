25b440ec66477ec90987795f719d9cb6d8be04b7
Romy M <rmar6544@gmail.com>
2021-04-01 17:32:29 +0000

    added comment, and code.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+#rollercoaster height requirments 
+# ask height is greater than 120cm can ride 
+# otherwise can not ride.
+
 print("Welcome to the rollercoaster!")
+
 height = int(input("What is your height in cm? "))
+
+if height > 120:
+   print("You can ride")
+
+else:
+  print("I'm sorry you can not ride")  
